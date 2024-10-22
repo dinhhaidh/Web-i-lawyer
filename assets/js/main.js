5afaab5e201c20149688dcadd29ee3924bf5fee8
dinhhaidh <phamhaiqt02@gmail.com>
2024-10-22 11:13:48 +0700

    push

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,12 +27,14 @@ const scrollActive = () =>{
                 sectionTop = current.offsetTop - 58,
                 sectionId = current.getAttribute('id'),
                 sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
-
-        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-        sectionsClass.classList.add('active-link')
-        }else{
-        sectionsClass.classList.remove('active-link')
-        }                                                    
+        if (sectionsClass !== null) {
+            if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+                sectionsClass.classList.add('active-link')
+            }else{
+                sectionsClass.classList.remove('active-link')
+            }      
+        }
+                                                     
     })
 }
 window.addEventListener('scroll', scrollActive)
